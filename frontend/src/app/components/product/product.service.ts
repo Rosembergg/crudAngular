a46848c7b9820ceecebf3caaab4f879e03a6f4b7
Rosembergg <rrosembergg.45@gmail.com>
2022-05-27 08:48:32 -0300

    Primeiro commit

---
+++ frontend/src/app/components/product/product.service.ts
@@ -0,0 +1,57 @@
+import { map, catchError} from 'rxjs/operators';
+import { Product } from './product.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { EMPTY, Observable } from 'rxjs';
+import { mixinColor } from '@angular/material/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseUrl = "http://localhost:3001/products"
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+
+  showMenssage(msg: string, isError: boolean = false): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      panelClass: isError ?['msg-erro']: ['msg-sucess']
+      
+    })
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product).pipe(map(obj => obj))
+    catchError(e=> this.erroHandler(e))
+  }
+  
+  erroHandler(e:any): Observable<any>{
+      this.showMenssage('Erro, entre em contato com o FUNDADOR', true)
+      return EMPTY
+  }
+
+  read():Observable<Product[]>{
+    return this.http.get<Product[]>(this.baseUrl)
+  }
+
+  readById(id: string):Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+
+  delete(id: number): Observable <Product>{
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url)
+  }
+}     
